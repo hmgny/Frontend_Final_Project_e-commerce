8cd3936064df00e80f01ee48ff677f16a206fee0
hmgny <hmerveguney@hotmail.com>
2024-12-11 09:30:45 +0300

    Home sayfası için route oluşturuldu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import React from "react";
+import HomePage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+      <Route path="/" exact component={HomePage} />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
