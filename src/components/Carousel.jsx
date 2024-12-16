bdbebc01b790dcc10f6e5cb515dfc0ee4ddacdaa
hmgny <hmerveguney@hotmail.com>
2024-12-17 00:32:12 +0300

    carousel componentinin tasarımı tamamlandı

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const sliderImages = [
   "/images/men_png_yesil.png",
-  "/images/best1.jpg",
-  "/images/best2.jpg",
+  "/images/men_png_yesil.png",
+  "/images/men_png_yesil.png",
 ];
 
 function Carousel() {
@@ -31,32 +31,39 @@ function Carousel() {
   };
 
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative  w-full h-[50%] overflow-hidden">
       {/* Kaydırılabilir içerik */}
-      <div className="flex transition-all duration-500 ease-in-out">
+      <div className="flex transition-all duration-500 ease-in-out ">
         {/* Slider içeriği */}
         {sliderImages.map((image, index) => (
           <div
             key={index}
-            className="flex-shrink-0 w-full h-screen flex flex-col gap-8 justify-center items-center sm:items-start pt-56 sm:px-44 bg-Secondary text-lightTextColor"
+            className="flex-shrink-0  w-full h-full flex flex-col gap-8 justify-center pt-52 sm:pt-0  bg-Secondary text-lightTextColor"
             style={{ display: index === currentIndex ? "flex" : "none" }} // Yalnızca geçerli slayt gösterilsin
           >
-            <div className="flex flex-col gap-8 max-w-64 sm:max-w-none">
-              <h5 className="h5 text-base">SUMMER 2020</h5>
-              <h2 className="h2 text-4xl font-bold">NEW COLLECTION</h2>
-              <h4 className="text-xl text-lightGray">
-                We know how large objects will act, but things on a small scale.
-              </h4>
-              <h4 className="h4 text-base font-bold">$16.48</h4>
-              <button className="bg-succes text-lightTextColor text-3xl h7 font-bold w-56 h-16 rounded-md">
-                ADD TO CART
-              </button>
+            <div className="flex flex-col items-center sm:flex-row sm:justify-around">
+              <div className="flex flex-col gap-8 max-w-64 sm:max-w-96 items-center text-center sm:text-start sm:items-start ">
+                <h5 className="h5 text-base">SUMMER 2020</h5>
+                <h2 className="h2 text-4xl font-bold sm:h1 sm:leading-[80px]">
+                  Vita Classic Product
+                </h2>
+                <h4 className="text-xl text-lightGray ">
+                  We know how large objects will act, but things on a small
+                  scale.
+                </h4>
+                <div className="flex flex-col gap-6 sm:flex-row">
+                  <h4 className="h4 text-base font-bold">$16.48</h4>
+                  <button className="bg-succes text-lightTextColor text-3xl h7 font-bold w-56 h-16 rounded-md">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+              <img
+                src={image}
+                alt={`Carousel Image ${index + 1}`}
+                className=" object-top object-cover h-[600px] mt-8 sm:mt-0 sm:pt-20  sm:h-3/4"
+              />
             </div>
-            <img
-              src={image}
-              alt={`Carousel Image ${index + 1}`}
-              className="object-top object-cover w-full h-[600px] sm:h-[800px] mt-8"
-            />
           </div>
         ))}
       </div>
