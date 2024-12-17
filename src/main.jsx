f2e66710d73a3a7036182413309f31c839a9f169
hmgny <hmerveguney@hotmail.com>
2024-12-17 20:21:13 +0300

    main.js de App provider ile sarıldı.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-    <ToastContainer />
-  </StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+      <ToastContainer />
+    </Provider>
+  </React.StrictMode>
 );
