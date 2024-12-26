ce3d6cd152aaa4960bef685a7cf5ac36aa746bde
hmgny <hmerveguney@hotmail.com>
2024-12-27 00:39:15 +0300

    sepet üzerinde görülen ürün çeşiti sayısı, toplam ürün sayısı olarak değiştirildi

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -192,7 +192,7 @@ function Header() {
                 <ShoppingCart className="text-black sm:text-Primary" />
                 {cart.length > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
-                    {cart.length}
+                    {cart.reduce((total, item) => total + item.count, 0)}
                   </span>
                 )}
               </button>
